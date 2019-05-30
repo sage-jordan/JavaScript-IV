814be46ea09df04a80f326eb3872112ba4936cfe
sage-jordan <sagemjrodan@gmail.com>
2019-05-30 18:44:41 -0400

    Finished classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,12 +1,12 @@
 // CODE here for your Lambda Classes
-class Person{
-    constructor(name, age, location){
-    this.name = name;
-    this.age = age;
-    this.location = location;
+class Person {
+    constructor(attr){
+        this.name = attr.name;
+        this.age = attr.age;
+        this.location = attr.location;
     }
     speak(){
-        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
     }
 }
 class Instructor extends Person {
@@ -17,10 +17,10 @@ class Instructor extends Person {
         this.catchPhrase = attr.catchPhrase;
    }
    demo(subject){
-       console.log(`Today we are learnind about ${subject}`)
+        return `Today we are learnind about ${subject}`;
    }
    grade(student, subject){
-        console.log(`${student} receives a perfect score on ${subject}`)
+        return `${student} receives a perfect score on ${subject}`;
    }
 }
 class Student extends Person {
@@ -31,13 +31,13 @@ class Student extends Person {
         this.favSubjects = attr.favSubjects;
     }
     listSubjects(student){
-        console.log(`${student.favSubjects[0]}, ${student.favSubjects[1]}, ${student.favSubjects[2]}`)
+        return `${student.favSubjects[0]}, ${student.favSubjects[1]}, ${student.favSubjects[2]}`;
     }
     PRAssignment(subject){
-        console.log(`${student.name} has submitted a PR for ${subject}`);
+        return `${this.name} has submitted a PR for ${subject}`;
     }
     sprintChallenge(subject){
-        console.log(`${student.name} has begun sprint challenge on ${subject}`);
+        return `${this.name} has begun sprint challenge on ${subject}`;
     }
 }
 class ProjectManager extends Instructor {
@@ -47,27 +47,39 @@ class ProjectManager extends Instructor {
         this.favInstructor = attr.favInstructor;
     }
     standUp(channel){
-        console.log(`${ProjectManager.name} announces to ${channel}, @channel standy times!`);
+        return `${this.name} announces to ${channel}, @channel standy times!`;
     }
     debugsCode(studentObj, subject){
-        `${ProjectManager.name} debugs ${studentObj.name}'s code on ${subject}.`
+        return `${this.name} debugs ${studentObj.name}'s code on ${subject}.`;
     }
 }
-const fred = new Instructor({
-    name: 'Fred',
-    location: 'Bedrock',
-    age: 37,
-    favLanguage: 'JavaScript',
-    specialty: 'Front-end',
-    catchPhrase: `Don't forget the homies`
-  });
-const sage = new Student({
-    name: "Sage",
-    age: 20,
-    location: "Indianapolis",
-    previousBackground: "Host",
-    className: "WEBPT7",
-    favSubjects: ["Math", "Science", "Computers"]
-});
-  console.log(fred.favLanguage);
-  console.log(fred.debugsCode(sage, "Javascript"));
+    const fred = new Instructor({
+        name: 'Fred',
+        location: 'Bedrock',
+        age: 37,
+        favLanguage: 'JavaScript',
+        specialty: 'Front-end',
+        catchPhrase: `Don't forget the homies`
+    });
+    const sage = new Student({
+        name: "Sage",
+        age: 20,
+        location: "Indianapolis",
+        previousBackground: "Host",
+        className: "WEBPT7",
+        favSubjects: ["Math", "Science", "Computers"]
+    });
+    const phil = new ProjectManager({
+        name: "Phil",
+        age: 25,
+        location: "Denver",
+        gradClassName: "not sure", 
+        favInstructor: "Dan"
+    });
+console.log(fred.name); // Fred
+console.log(sage.name); // Sage
+console.log(sage.speak()); // Hello my name is Sage, I am from Indianapolis
+console.log(sage.className); // WEBPT7
+console.log(sage.PRAssignment("JavascriptIII")); // Sage has submitted a PR for JavascriptIII
+console.log(fred.grade(sage.name, "JavascriptIII")); // Sage receives a perfect score on JavascriptIII
+console.log(phil.debugsCode(sage, "JavascriptIII")); // Phil debugs Sage's code on JavascriptIII.
